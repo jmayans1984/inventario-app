9fc4948548c234ab777a4599b4842309b051d22e
Juan Mayans <juan.donado84@gmail.com>
2026-05-17 00:10:51 -0400

    fix: Configure correct base path for COMPLETA version

--- inventario-vue/vite.config.js
+++ inventario-vue/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
+  base: '/inventario-app/completa/',
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
