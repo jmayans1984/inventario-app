35859c388ac886c5d957756ce3e17a1eaba9c54f
Juan Mayans <juan.donado84@gmail.com>
2026-05-26 07:31:43 -0400

    build: rebuild frontend con chips de grupo y sin campos de precios

--- completa/assets/CuentasBancariasView-7TQjvU7b.js
+++
@@ -1,1 +0,0 @@
-import{Gt as e,an as t,r as n,rn as r}from"./index-D-V5Y2S9.js";var i={__name:`CuentasBancariasView`,setup(i){let a=n();return r(()=>{a.push(`/contabilidad/configuracion/cuentas-bancarias`)}),(n,r)=>(t(),e(`div`))}};export{i as default};
